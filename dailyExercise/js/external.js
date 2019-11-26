df32d0ed853f2f3c801144d1692b676d9c49f46b
John Santos <johnsantos@Johns-MacBook-Pro.local>
2019-11-26 09:31:17 -0600

    daily js pushup and cleanup

---
+++ dailyExercise/js/external.js
@@ -0,0 +1,19 @@
+/* javascript with html exercises*/
+
+// ============= 1-7 ==========================
+// console message to test external link
+console.log("Hello from inline JavaScript");
+
+// ============= 1 ==========================
+// Alert message
+alert("'Welcome to my Website!'.");
+
+// ============= 2 ==========================
+// Prompt user for input
+var favoriteColor = prompt('What is your favorite color?');
+
+// Include user input from prompt in alert message
+alert(" Greate, " + favoriteColor + " is my favorite color too!");
+
+
+// ============= 3 ==========================
