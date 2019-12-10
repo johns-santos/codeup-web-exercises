3e16fc286666ee5784f5f0c965f60e8cf0f393f0
johns-santos <johnsantos@Johns-MacBook-Pro.local>
2019-12-10 09:32:28 -0600

    new array branch

--- dailyExercise/js/break_and_continue.js
+++ dailyExercise/js/break_and_continue.js
@@ -1,4 +1,3 @@
-"use strict";
 
 do {
     var userNumber = Number(prompt("Pick an odd number between 1 and 50"));
@@ -10,19 +9,19 @@ do {
 
         alert(userNumber + " is not between 1 and 50, please pick again");
 
-    }else if (isNaN(userNumber)) {
+    } else if (isNaN(userNumber)) {
         alert(userNumber + " is not a number, please pick again");
 
-    }else {
+    } else {
         alert("congrats you picked the right number");
         break;
     }
 } while (true)
 
 //version 1
-for(var i = 1; i <= 50; i+=2){
+for (var i = 1; i <= 50; i += 2) {
 
-    if(i === userNumber){
+    if (i === userNumber) {
         console.log("Yikes! Skipping number: " + i);
         continue;
     }
@@ -45,3 +44,25 @@ for (var i = 1; i <= 50; i++) {
     }
 
 }
+
+(function() {
+    "use strict";
+
+
+
+// WHILE
+    console.log('=========WHILE========');
+    var skipNumber;
+
+    while (true) {
+        skipNumber = Number(prompt("Please enter an odd number between 1 and 50: "));
+        if(skipNumber % 2 !== 0 && skipNumber >= 1 && skipNumber <= 50) {
+            console.log("Number to skip is: " + skipNumber);
+            break;
+        }
+    }
+
+
+
+
+})();
