c6ec1d9f3e7c9cd39ce6ea155e4c0007d87a422b
johns-santos <johnsantos@Johns-MacBook-Pro.local>
2020-04-25 03:32:11 -0500

    added random password generater function - generateP

--- javaScriptPrac/js101/function.js
+++ javaScriptPrac/js101/function.js
@@ -1,33 +1,45 @@
+// Running file with node.js
+// Example: node <file.name>
+// node function.js
+
 
 console.log("==singSong===");
-function singSong(){
+
+function singSong() {
     console.log("tet");
     console.log("tat");
     console.log("tet");
     console.log("tat");
 }
+
 singSong();
 
 
 console.log("==sayHello==");
-function sayHello(name){
+
+function sayHello(name) {
     console.log("Hello " + name)
 }
+
 sayHello("john");
 
 console.log("==area==");
-function area(l, w){
+
+function area(l, w) {
     console.log("The area = " + l * w)
 }
+
 area(2, 10);
 
 
 console.log("==greet==");
-function greet(person1, person2, person3){
+
+function greet(person1, person2, person3) {
     console.log("Hello " + person1);
     console.log("Hello " + person2);
     console.log("Hello " + person3);
 }
+
 greet("john", "bella", "Irene");
 
 
@@ -36,24 +48,27 @@ console.log("======");
 console.log("======");
 console.log("===Using RETURN===");
 
-function square(x){
-    return x*x;
+function square(x) {
+    return x * x;
 }
 
 var result = square(3);
-console.log(result/2);
+console.log(result / 2);
 
 
 console.log("===capitalize===");
-function capitalize(str){
+
+function capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
 var city = "paris";
 var capital = capitalize(city);
 console.log(capital);
 
 
 console.log("===Use RETURN to stop function===");
+
 function cap(str1) {
     if (typeof str1 === "number") {
         return "ERROR - That is not a string"
@@ -70,44 +85,44 @@ var capital1 = cap(num);
 console.log(capital1);
 
 console.log("=== Is Even ===");
-function isEven(num){
-    if(num % 2 === 0){
+
+function isEven(num) {
+    if (num % 2 === 0) {
         console.log("true");
     } else (console.log("false"));
 }
+
 console.log(isEven(4));
 console.log(isEven(21));
 console.log(isEven(68));
 console.log(isEven(333));
 
 
-
-
 console.log("=== Is Even REFACTORED===");
-function isEven2(num){
+
+function isEven2(num) {
     return num % 2 === 0;
 }
+
 console.log(isEven2(4));
 console.log(isEven2(10));
 console.log(isEven2(15));
 
 
-
-
-
 console.log("=== Is Factorial ===");
 
 
-function factorial(num){
+function factorial(num) {
     // define result
     var result = 1;
     // calculate factorial and store value in result
-    for(var i = 1; i <= num; i++) {
+    for (var i = 1; i <= num; i++) {
         result = result * i;
     }
     // return result
     return result;
 }
+
 console.log(factorial(3));
 console.log(factorial(4));
 console.log(factorial(0));
@@ -115,11 +130,47 @@ console.log(factorial(10));
 
 
 console.log("=== Is kebabToSnake ===");
+
 function kebabToSnake(str) {
     //replace all dasshes with underscore
     var newStr = str.replace(/-/g, "_");
     // return str
     return newStr
 }
+
 console.log(kebabToSnake("hello-world"));
 console.log(kebabToSnake("dogs-are-awesome"));
+
+
+console.log("====== Set Interval ======");
+
+function sing2() {
+    console.log("Twinkle, Twinkle, Little Star")
+    console.log("How I wonder what you are")
+}
+
+/*
+setInterval(sing2, 3000);
+*/
+
+
+//Generates random strings in two passes and concatenates results to form 1 secure password
+function generateP() {
+    var pass = '';
+    var pass1 = '';
+    var str = 'ABCDEFGHIJKLMNO';
+    var str1 = '0123456789@!#$%&*' + 'abcdefghijklmnopqrstuvwxyz' + 'PQRSTUVWXYZ';
+
+    for (i = 1; i <= 2; i++) {
+        var char = Math.floor(Math.random()
+            * str.length + 1);
+        pass += str.charAt(char);
+    }
+    for (i = 1; i <= 7; i++) {
+        var char1 = Math.floor(Math.random()
+            * str1.length + 1);
+        pass1 += str1.charAt(char1);
+    }
+    return pass + pass1;
+}
+console.log(generateP());
