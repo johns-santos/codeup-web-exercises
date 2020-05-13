b6163c0341d0f86ce6ac8a00a7211900aaf919a3
johns-santos <johnsanthony.santos@gmail.com>
2020-05-13 03:57:35 -0500

    added js animation

--- javaScriptPrac/websiteTemplate/resources/js/jScript.js
+++ javaScriptPrac/websiteTemplate/resources/js/jScript.js
@@ -1,35 +1,49 @@
 $(document).ready(function () {
 
-    /* === waypoint for sticky-nav bar EXAMPLE */
-    // var waypoints = $('#handler-first').waypoint(function(direction) {
-    //     notify(this.element.id + ' hit 25% from top of window')
-    // }, {
-    //     offset: '25%'
-    // })
     $('.js--section-features').waypoint(function (direction) {
         if (direction === "down") {
             $('nav').addClass('sticky');
         } else {
             $('nav').removeClass('sticky')
         }
-    }, { offset: '60px'
+    }, {
+        offset: '60px'
     });
 
 
     /* Scroll on buttons */
-    $('.js--scroll-to-features').click(function(){
+    $('.js--scroll-to-features').click(function () {
         $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 1000)
     });
 
-    $('.js--scroll-to-form').click(function(){
+    $('.js--scroll-to-form').click(function () {
         $('html, body').animate({scrollTop: $('.js--section-form').offset().top}, 3000)
     });
 
-    $('.--js-scroll-to-form2').click(function(){
+    $('.--js-scroll-to-form2').click(function () {
         $('html, body').animate({scrollTop: $('.js--section-form').offset().top}, 2000)
     });
+
+
+    /* Animation on scroll */
+    $('.js--wp-1').waypoint(function (direction) {
+        $('.js--wp-1').addClass('animate__fadeIn','8s');
+    }, {
+        offset: '50%'
+    });
+
+
+    $('.js--wp-2').waypoint(function (direction) {
+        $('.js--wp-2').addClass('animate__fadeInUp', '8s');
+    }, {
+        offset: '50%'
+    });
+
+    $('.js--wp-3').waypoint(function (direction) {
+        $('.js--wp-3').addClass('animate__bounceIn');
+    }, {
+        offset: '50%'
+    });
+
+
 });
-
-
-
-// section-form
