5f0854530596544602455226860fca8349bcdd9b
johns-santos <johnsanthony.santos@gmail.com>
2020-05-17 02:26:27 -0500

    js practice

--- javaScriptPrac/modJScript/functions/functions_101.js
+++ javaScriptPrac/modJScript/functions/functions_101.js
@@ -7,13 +7,13 @@ console.log(greetUser("John"));
 
 
 // =================================
-let square = function(num){
-    return num*2;
+let square = function (num) {
+    return num * 2;
 };
 console.log(square(2));
 
 // =================================
-let square2 = function(num){
+let square2 = function (num) {
     let result = num * num;
     return result;
 };
@@ -26,9 +26,13 @@ console.log(value);
 
 // ======= Challenge =============
 // convert Fahreheit to celsius
-function convertToFahrenheit(x){
-    let celsius = (x -32) * 5/9;
-    return celsius + " celsius";
-}
-console.log(convertToFahrenheit(32));
-console.log(convertToFahrenheit(68));
+let convertToFahrenheit = function (x) {
+    let celsius = (x - 32) * 5 / 9;
+    return celsius;
+};
+
+let output = " celsius";
+let tempOne = convertToFahrenheit(32) + output;
+let tempTwo = convertToFahrenheit(68) + output;
+console.log(tempOne);
+console.log(tempTwo);
