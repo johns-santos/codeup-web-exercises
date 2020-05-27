b3fa1a32d757a6bce799e0d73b93e20c83c20962
johns-santos <johnsanthony.santos@gmail.com>
2020-05-26 23:28:33 -0500

    string methods

--- javaScriptPrac/modJScript/objects/string-methods.js
+++ javaScriptPrac/modJScript/objects/string-methods.js
@@ -19,3 +19,20 @@ console.log(password.includes('word'));
 
 // Trim method - Removes white space...
 console.log(name.trim(2));
+
+
+// isValidPassword
+// return TRUE if password is more than 8 characters
+// does not contain word password
+
+
+
+let  isValidPassword = function (password) {
+    if((password.length >= 8) && (password.includes('password') !== true)){
+    return true;
+    }
+    else return false;
+};
+console.log(isValidPassword('123passwordRFG'));
+console.log(isValidPassword('123PAsswordRFG'));
+console.log(isValidPassword('123testRFG'));
