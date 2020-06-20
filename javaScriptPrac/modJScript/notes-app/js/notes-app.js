8bb97b23958b3772af2046c80b6c3a9ef5416598
johns-santos <johnsanthony.santos@gmail.com>
2020-06-20 01:03:40 -0500

    use querySelector to modify objects in DOM

--- javaScriptPrac/modJScript/notes-app/js/notes-app.js
+++ javaScriptPrac/modJScript/notes-app/js/notes-app.js
@@ -1,1 +1,27 @@
-console.log('test test');
+/* ==========
+DOM - Document Object Model
+============ */
+
+
+const p = document.querySelector('p');
+// console.log(p);
+
+// REMOVE method
+//=================================
+p.remove(); // removes the first "p" tag
+
+
+
+// Query all elements by class
+//=================================
+const test = document.querySelectorAll('.test')
+
+// use forEach to loop through all selected elements and update
+test.forEach(function(test) {
+    // console.log(test.textContent);
+    test.textContent = '**********'
+
+})
+
+
+
