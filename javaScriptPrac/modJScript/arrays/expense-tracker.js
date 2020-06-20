5c0681d6f693f56e901f255e1b745ea47319a271
johns-santos <johnsanthony.santos@gmail.com>
2020-06-20 00:16:21 -0500

    expense and income tracker using arrays

--- javaScriptPrac/modJScript/arrays/expense-tracker.js
+++ javaScriptPrac/modJScript/arrays/expense-tracker.js
@@ -1,27 +1,54 @@
 const account = {
     name: 'John Santos',
     expenses: [],
+    income: [],
     addExpense: function(description, amount){
         this.expenses.push({
             description: description,
             amount: amount
         })
     },
+    addIncome: function(description, amount){
+        this.income.push({
+            description: description,
+            amount: amount
+        })
+    },
     getAccountSummary: function(){
-        let totalExpenses = 0
+        let totalExpenses = 0;
+        let totalIncome = 0;
+        let accountBalance = 0;
 
         this.expenses.forEach(function (expense){
             totalExpenses = totalExpenses + expense.amount
         })
-        
-        return `${this.name} has $${totalExpenses} in expenses.`
+        this.income.forEach(function (income){
+            totalIncome = totalIncome + income.amount
+        })
+
+        accountBalance = totalIncome - totalExpenses;
+
+        return `${this.name} has a blance of $${accountBalance}. $${totalIncome} in income and $${totalExpenses} in expenses.`
     }
 }
 
-// Expense -> description(string) amount(num)
-// addExpense -> description, amount
-// getAccountSummary -> total up all expenses -> John Santos has $1250 in expenses.
+/* ===============================
+ CHALLENGE ------
+==================================
+ -  Expense -> description(string) amount(num)
+ - addExpense -> description, amount
+ - getAccountSummary -> total up all expenses -> John Santos has $1250 in expenses.
 
-account.addExpense('Rent', 950)
-account.addExpense('Coffee', 2)
+ STEPS
+ 1. Add income array to account
+ 2. addIncome method -> description, amount
+ 3. Tweak getAccountSummary
+
+OUTPUT:  John Santos has a balance of $10. $100 in income. $90 in expenses
+================================== */
+
+account.addExpense('Rent', 990)
+account.addExpense('Office Supplies', 100)
+account.addIncome('Job', 1000)
+account.addIncome('Rental Property', 1500)
 console.log(account.getAccountSummary())
