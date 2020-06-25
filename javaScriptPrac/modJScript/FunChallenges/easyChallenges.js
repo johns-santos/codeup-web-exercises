54e7c8a90f8df41ea18b39652c19524f7fa0e363
johns-santos <johnsanthony.santos@gmail.com>
2020-06-25 02:25:11 -0500

    added new js challenge

--- javaScriptPrac/modJScript/FunChallenges/easyChallenges.js
+++ javaScriptPrac/modJScript/FunChallenges/easyChallenges.js
@@ -1,7 +1,7 @@
 function palindrome(str) {
 
     // Regular expression - sequence of characters that match a search pattern "/[\W_]/g" remove all non-numeric and underscores
-    var reg = /[W0-9_]/g;
+    var reg = /[W0-9_*]/g;
 
     // Match all RegEx and replace with nothing
     var smallStr = str.toLowerCase().replace(reg, "");
@@ -22,5 +22,28 @@ console.log(palindrome('dad_12'));
 console.log(palindrome('123eye'));
 console.log(palindrome('dad'));
 console.log(palindrome('test'));
-console.log(palindrome('dadadadadadf'));
+console.log(palindrome('dadadadadad*'));
 
+
+
+console.log('========================');
+
+
+function longestWord (str) {
+   
+    // convert str to array of words
+    var words = str.split(" ");
+
+    // Before loop create an empty object
+    var longest = "";
+
+    // loop through words
+    for(var word of words){
+        // 1st word in str will be longest than empty "longest"
+        // then 1st word will be compared to next word
+        if(word.length > longest.length) longest = word;
+    }
+    return longest.length
+}
+
+console.log(longestWord('This is a test sentence'));
